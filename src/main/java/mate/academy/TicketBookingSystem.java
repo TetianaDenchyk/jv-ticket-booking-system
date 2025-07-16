a11c6d370ca4bdcd22e51c19ec43b669cd6b52d1
tetiana <tetiana.denchyk@ocado.com>
2025-07-16 16:40:07 +0300

    completed task

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,32 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TicketBookingSystem {
 
-    public TicketBookingSystem(int totalSeats) {
+    private Semaphore semaphore;
+    private AtomicInteger totalSeats;
 
+    public TicketBookingSystem(int totalSeats) {
+        this.totalSeats = new AtomicInteger(totalSeats);
+        semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        BookingResult result;
+        try {
+            semaphore.acquire();
+            if (totalSeats.get() > 0) {
+                totalSeats.decrementAndGet();
+                result = new BookingResult(user, true, "Booking successful.");
+            } else {
+                result = new BookingResult(user, false, "No seats available.");
+            }
+            semaphore.release();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 }
